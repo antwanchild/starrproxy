596070235dc0fce2891e825e5a339517727b5f1f
Austin Best <austin.w.best@gmail.com>
2024-10-25 16:10:04 -0400

    Fix adding templates not working after restructure

--- root/app/www/public/js/starr.js
+++ root/app/www/public/js/starr.js
@@ -129,41 +129,6 @@ function deleteAppStarrAccess(app, id)
     }
 }
 // -------------------------------------------------------------------------------------------
-function openTemplateStarrAccess(app, id)
-{
-    $.ajax({
-        type: 'POST',
-        url: 'ajax/starr.php',
-        data: '&m=openTemplateStarrAccess&app=' + app + '&id=' + id,
-        success: function (resultData) {
-            dialogOpen({
-                id: 'openTemplateStarrAccess',
-                title: 'Create new template for ' + app,
-                size: 'lg',
-                body: resultData
-            });
-        }
-    });
-}
-// -------------------------------------------------------------------------------------------
-function saveTemplateStarrAccess(app, id)
-{
-    if (!$('#new-template-name').val()) {
-        toast('Templates', 'Template name is required', 'error');
-        return;
-    }
-
-    $.ajax({
-        type: 'POST',
-        url: 'ajax/starr.php',
-        data: '&m=saveTemplateStarrAccess&app=' + app + '&id=' + id + '&name=' + encodeURIComponent($('#new-template-name').val()),
-        success: function () {
-            dialogClose('openTemplateStarrAccess');
-            toast('Templates', 'The template has been added', 'info');
-        }
-    });
-}
-// -------------------------------------------------------------------------------------------
 function resetUsage(app, id)
 {
     if (confirm('Are you sure you want to reset the usage counter?')) {
--- root/app/www/public/js/templates.js
+++ root/app/www/public/js/templates.js
@@ -61,3 +61,38 @@ function deleteCustomTemplate(app, starr)
     }
 }
 // ---------------------------------------------------------------------------------------------
+function openTemplateStarrAccess(app, id)
+{
+    $.ajax({
+        type: 'POST',
+        url: 'ajax/templates.php',
+        data: '&m=openTemplateStarrAccess&app=' + app + '&id=' + id,
+        success: function (resultData) {
+            dialogOpen({
+                id: 'openTemplateStarrAccess',
+                title: 'Create new template for ' + app,
+                size: 'lg',
+                body: resultData
+            });
+        }
+    });
+}
+// -------------------------------------------------------------------------------------------
+function saveTemplateStarrAccess(app, id)
+{
+    if (!$('#new-template-name').val()) {
+        toast('Templates', 'Template name is required', 'error');
+        return;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'ajax/templates.php',
+        data: '&m=saveTemplateStarrAccess&app=' + app + '&id=' + id + '&name=' + encodeURIComponent($('#new-template-name').val()),
+        success: function () {
+            dialogClose('openTemplateStarrAccess');
+            toast('Templates', 'The template has been added', 'info');
+        }
+    });
+}
+// -------------------------------------------------------------------------------------------
